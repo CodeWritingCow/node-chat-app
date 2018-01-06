a31343eb3cf88ab0531ea2f5e7e3c5a817005211
Gary Pang <gary.pang@gmail.com>
2018-01-06 20:23:12 +0000

    Add index.js left out of previous commit

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to server');
+    
+    socket.emit('createEmail', {
+       to: 'totoro@totoro.com',
+       text: 'Moo?'
+    });
+});
+
+socket.on('disconnect', function  () {
+    console.log('Disconnected from server');
+});
+
+socket.on('newEmail', function(email) {
+   console.log('New email', email);
+});
